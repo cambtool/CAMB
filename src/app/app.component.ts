04102b52c96d8327859f0164a1f667052fe6963a
Mubasher-Munir <sheikh.mubasher.munir@gmail.com>
2024-01-20 20:01:25 +0500

    removing deployment folder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { LoaderService } from './Loader.service';
+import { APP_BASE_HREF } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [{ provide: APP_BASE_HREF, useValue: '/CAMB/' }]
 })
 export class AppComponent {
   constructor(public loaderService: LoaderService, private http: HttpClient) { }
